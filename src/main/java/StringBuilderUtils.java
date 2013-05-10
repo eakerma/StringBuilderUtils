5d0c279a3d52073b6cc60b8c79ac16ef62a03ec9
Michael Akerman <mike@uark.edu>
2013-05-10 09:54:51 -0500

    Added src, pom.xml, and .gitignore

---
+++ src/main/java/StringBuilderUtils.java
@@ -0,0 +1,24 @@
+package edu.uark.util;
+
+import java.util.*;
+import org.apache.commons.lang.Validate;
+
+public final class StringBuilderUtils
+{
+	//StringBuilder doesn't have a decent replace method
+	public static void replaceAll( final StringBuilder sb, final String search, final String replace )
+	{
+		Validate.notNull( sb );
+		Validate.notEmpty( search );
+		Validate.notNull( replace );
+	
+		while ( true )
+		{
+			final int start = sb.indexOf( search );
+			if ( start == -1 ) break;
+			
+			final int end = start + search.length();
+			sb.replace( start, end, replace );
+		}
+	}
+}
---
+++ src/test/java/StringBuilderUtilsTest.java
@@ -0,0 +1,24 @@
+package edu.uark.util;
+
+import java.io.*;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ * @author Michael Akerman
+ */
+public class StringBuilderUtilsTest
+{
+	@Test
+	public void testReplaceAll () throws IOException
+	{
+		final StringBuilder sb = new StringBuilder( "tacos" );
+		StringBuilderUtils.replaceAll( sb, "t", "+" );
+		StringBuilderUtils.replaceAll( sb, "a", "@" );
+		StringBuilderUtils.replaceAll( sb, "o", "0" );
+		StringBuilderUtils.replaceAll( sb, "s", "$" );
+		
+		assertEquals( "+@c0$", sb.toString() );
+	}
+}
